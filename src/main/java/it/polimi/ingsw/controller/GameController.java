14cbbae38189c8aad5baad97fa6687144ccaa664
Luca <luca.maniscalchi@mail.polimi.it>
2020-04-06 22:29:35 +0200

    Add basic GameController

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -2,15 +2,15 @@ package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.controller.turn_controllers.*;
 import it.polimi.ingsw.model.Game;
-import it.polimi.ingsw.model.cards.Card;
 import it.polimi.ingsw.model.cards.Deck;
+import it.polimi.ingsw.model.players.Player;
 
 import java.util.ArrayList;
 
 public class GameController {
 
     private Game game;
-    GodController apolloController,artemisController,athenaController,atlasController,demeterController,hephaestusController,hermesController,limusController,minotaurController,panController,prometheusController,zeusController;
+    GodController apolloController,artemisController,athenaController,atlasController,demeterController,hephaestusController,limusController,minotaurController,panController,prometheusController,zeusController;
 
     public GameController(Game game) {
         this.game = game;
@@ -22,7 +22,9 @@ public class GameController {
 
     public void gameSetUp(){
         ArrayList<GodController> controllers;
-        Deck deck;
+        Deck deck = game.getDeck();
+        String result;
+        int i;
 
         apolloController = new ApolloController(this);
         artemisController = new ArtemisController(this);
@@ -30,14 +32,12 @@ public class GameController {
         atlasController = new AtlasController(this);
         demeterController = new DemeterController(this);
         hephaestusController = new HephaestusController(this);
-        hermesController = new HermesController(this);
         limusController = new LimusController(this);
         minotaurController = new MinotaurController(this);
         panController = new PanController(this);
         prometheusController = new PrometheusController(this);
         zeusController = new ZeusController(this);
         controllers = new ArrayList<GodController>();
-        deck = new Deck();
 
         controllers.add(apolloController);
         controllers.add(artemisController);
@@ -45,7 +45,6 @@ public class GameController {
         controllers.add(atlasController);
         controllers.add(demeterController);
         controllers.add(hephaestusController);
-        controllers.add(hermesController);
         controllers.add(limusController);
         controllers.add(minotaurController);
         controllers.add(panController);
@@ -54,6 +53,28 @@ public class GameController {
 
         for (GodController godController : controllers){
             deck.addCard(godController.generateCard());
+        }
+
+        deck.pickRandom(game.getPlayerNum());
+        i=0;
+
+        for(Player player : game.getPlayers())
+        {
+            player.setGodCard(deck.getPickedCards().get(i));
+            i++;
+        }
+
+        // Gestisco i turni
+        while(!game.hasWinner()){
+            result = game.getActivePlayer().getGodCard().getController().playTurn();
+            if(result.equals("NEXT"))
+                game.getNextPlayer(game.getActivePlayer());
+            else
+                if (result.equals("LOST"))
+                    game.setWinner(game.getNextPlayer(game.getActivePlayer()));
+                else
+                    if(result.equals("WON"))
+                        game.setWinner(game.getActivePlayer());
         }
 
     }
