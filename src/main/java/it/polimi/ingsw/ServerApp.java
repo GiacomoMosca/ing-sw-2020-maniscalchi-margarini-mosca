9b56a4306c0c30b85f2542ed3654b8bfbdba5154
GiacomoMosca <10574012@polimi.it>
2020-06-29 23:06:00 +0200

    Properly set logger in GameController

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.network.server.Server;
 /**
  * Allows starting a new Game, creating an instance of the Server class.
  */
-public class TestApp {
+public class ServerApp {
 
     public static void main(String[] args) {
         Server server = new Server(8000);
