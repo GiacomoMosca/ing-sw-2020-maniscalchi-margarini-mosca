7ae7e1711dd9b3bf9a217adfb5a2a7fd6ce0f572
Luca <luca.maniscalchi@mail.polimi.it>
2020-07-03 17:44:19 +0200

    Fix GameControllerTest

---
+++ src/test/java/it/polimi/ingsw/network/server/FakeLogger.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.network.server;
+
+import java.io.IOException;
+
+public class FakeLogger extends Logger {
+
+    public FakeLogger() throws IOException {
+    }
+
+    @Override
+    public void logError(String s) {
+        //
+    }
+}
